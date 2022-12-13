33500061c7a44ab745a15b120be96608c8e59254
shadowkiller33 <2683142147@qq.com>
2022-12-13 16:48:05 -0500

    renew

--- src/text2csv.py
+++ src/text2csv.py
@@ -1,17 +1,32 @@
 import csv
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--train_file_name", type=str, default=1)
+parser.add_argument("--test_file_name", type=str, default=1)
+args = parser.parse_args()
 
-with open('train.txt', encoding='utf-8') as txtfile:
+train_name = args.train_file_name
+test_name = args.test_file_name
+import pandas as pd
+jsonObj = pd.read_json(path_or_buf= str(train_name)+'.jsonl', lines=True)
+with open(str(train_name)+'.txt','w', encoding='utf-8') as f:
+    for i in range(len(jsonObj)):
+        f.write(jsonObj['prompt'][i] + jsonObj['completion'][i])
+with open(str(train_name)+'.txt', encoding='utf-8') as txtfile:
     all_text = txtfile.read()
-with open('train.csv', mode='w', encoding='utf-8') as csv_file:
+with open(str(train_name)+'.csv', mode='w', encoding='utf-8') as csv_file:
     fieldnames = ['text']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
     writer.writerow({'text': all_text})
 
-
-with open('validation.txt', encoding='utf-8') as txtfile:
+jsonObj = pd.read_json(path_or_buf= str(test_name)+'.jsonl', lines=True)
+with open(str(test_name)+'.txt','w', encoding='utf-8') as f:
+    for i in range(len(jsonObj)):
+        f.write(jsonObj['prompt'][i] + jsonObj['completion'][i])
+with open(str(test_name)+'.txt', encoding='utf-8') as txtfile:
     all_text = txtfile.read()
-with open('validation.csv', mode='w', encoding='utf-8') as csv_file:
+with open(str(test_name)+'.csv', mode='w', encoding='utf-8') as csv_file:
     fieldnames = ['text']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
